6d3dcc191da6b17d36e81adc0430d4e2a563b93c
Farlee <farleeh@Gmail.COM>
2015-04-23 22:50:24 +0800

    Update updateipfile.php

--- updateipfile.php
+++ updateipfile.php
@@ -1,9 +1,9 @@
 <?php  
-  $path = '/home/harry/Desktop/tmp/';
-  $target_path = '/srv/www/script';  #mus be writable by web server
+  $source_path = '/home/harry/Desktop/tmp/';
+  $target_path = '/srv/www/script';  #must be writable by web server
 
-  $copywrite = file_get_contents($path."copywrite.rar");
-  $qqwry = file_get_contents($path."qqwry.rar");
+  $copywrite = file_get_contents($source_path."copywrite.rar");
+  $qqwry = file_get_contents($source_path."qqwry.rar");
   $key = unpack("V6", $copywrite);
   $key = $key[6];
   for($i=0; $i<0x200; $i++)  {
@@ -19,5 +19,5 @@
   if($fp)  {
 	  fwrite($fp, $qqwry);
 	  fclose($fp);
-  } 
+  }
 ?>
