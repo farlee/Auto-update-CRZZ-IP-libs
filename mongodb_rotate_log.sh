3d6e8dc8d594437d7e680f65ef43d50b2fe5ab08
Farlee <farleeh@Gmail.COM>
2016-04-06 14:44:58 +0800

    Update mongodb_rotate_log.sh

--- mongodb_rotate_log.sh
+++ mongodb_rotate_log.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #example: /usr/local/mongodb/bin/mongod --fork --dbpath=/data/mongodb/ --logpath=/usr/local/mongodb/mongodb.log --logappend
 datafile=/data/mongodb/
-logfile=/usr/local/mongodb/mongodb.log
+logfile=/usr/local/mongodb
 days=7 #retain the lastest 7 days logs
 /bin/kill -SIGUSR1 `cat $datafile/mongod.lock` #rotate
 find $logfile/mongodb.log.* -type f -mtime +$days -delete
-#Remember: chmod 755 mongodb_rotate_log.sh
+#Remember: chmod +x mongodb_rotate_log.sh
