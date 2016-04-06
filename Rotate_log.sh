9d8fcc959ad2f3b36132af0e67df9de572c04fee
Farlee <farleeh@Gmail.COM>
2016-04-06 18:47:53 +0800

    Update and rename mongodb_rotate_log.sh to Rotate_log.sh

--- Rotate_log.sh
+++ Rotate_log.sh
@@ -6,3 +6,19 @@ days=7 #retain the lastest 7 days logs
 /bin/kill -SIGUSR1 `cat $datafile/mongod.lock` #rotate
 find $logfile/mongodb.log.* -type f -mtime +$days -delete
 #Remember: chmod +x mongodb_rotate_log.sh
+
+
+
+Another rotate log for nginx:
+  vi /etc/logrotate.d/nginx:
+/home/wwwlogs/*.log {
+    daily
+    dateext
+    compress
+    rotate 30
+    sharedscripts
+    postrotate
+        [ ! -f /usr/local/nginx/logs/nginx.pid ] || kill -USR1 `cat /usr/local/nginx/logs/nginx.pid`
+        mv /home/wwwlogs/*.log-* /home/wwwlogs/backup/
+    endscript
+}
